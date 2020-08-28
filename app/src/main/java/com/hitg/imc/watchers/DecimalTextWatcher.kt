cf2b41bf45163815553b74a5282f8ff4c2b4e4f0
Helton Isac <helton.isac@gmail.com>
2020-08-27 21:58:09 -0300

    Implementação da classe DecimalTextWatcher

--- app/src/main/java/com/hitg/imc/watchers/DecimalTextWatcher.kt
+++ app/src/main/java/com/hitg/imc/watchers/DecimalTextWatcher.kt
@@ -2,17 +2,53 @@ package com.hitg.imc.watchers
 
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.EditText
+import java.lang.ref.WeakReference
+import java.math.BigDecimal
+import java.text.DecimalFormat
+import kotlin.math.pow
 
-class DecimalTextWatcher :TextWatcher {
-    override fun afterTextChanged(p0: Editable?) {
-        TODO("Not yet implemented")
+class DecimalTextWatcher(editText: EditText,
+                         private val totalDecimalNumber: Int = 2) : TextWatcher {
+    private val editTextWeakReference: WeakReference<EditText> = WeakReference(editText)
+
+    init {
+        formatDigits(editTextWeakReference.get()!!.text)
     }
 
-    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        TODO("Not yet implemented")
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+    override fun afterTextChanged(editable: Editable) {
+        formatDigits(editable)
     }
 
-    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        TODO("Not yet implemented")
+    private fun getTotalDecimalNumber(): String {
+        val decimalNumber = StringBuilder()
+        for (i in 1..totalDecimalNumber) {
+            decimalNumber.append("0")
+        }
+        return decimalNumber.toString()
+    }
+
+    private fun formatDigits(editable: Editable) {
+        val editText = editTextWeakReference.get() ?: return
+        val cleanString = editable.toString().trim().replace(" ", "")
+        editText.removeTextChangedListener(this)
+        val number = 10.toDouble().pow(totalDecimalNumber.toDouble())
+        val parsed = when (cleanString) {
+            "" -> BigDecimal(0)
+            "null" -> BigDecimal(0)
+            else -> BigDecimal(cleanString.replace("\\D+".toRegex(), ""))
+                .setScale(totalDecimalNumber, BigDecimal.ROUND_FLOOR)
+                .divide(
+                    BigDecimal(number.toInt()),
+                    BigDecimal.ROUND_FLOOR
+                )
+        }
+        val dfnd = DecimalFormat("#,##0.${getTotalDecimalNumber()}")
+        val formatted = dfnd.format(parsed)
+        editText.setText(formatted.replace(',', '.'))
+        editText.setSelection(formatted.length)
+        editText.addTextChangedListener(this)
     }
 }
